b08517101e444b08caeae8afb5370ba9042f5767
minyun02 <www5981@naver.com>
2021-12-07 19:18:20 +0900

    1207 - 완주하지못한선

--- algorithm_study/src/yun/Hash01.java
+++ algorithm_study/src/yun/Hash01.java
@@ -1,10 +1,32 @@
 package yun;
 
+import java.util.HashMap;
+import java.util.Map;
+
+//완주하지 못한 선수
 public class Hash01 {
+	public String findPlayer(String[] part, String[] comp) {
+		String answer = ""; 
+		HashMap<String, String> map = new HashMap<String, String>();
+		//참가선수를 맵에 다 넣고
+		for(String player : part) {
+			map.put(player, "참가");
+		}
+		//완주선수를 맵에서 찾아서 뺀다
+		for(String player : comp) {
+			map.remove(player);
+		}
+		//맵에 남아있는 선수이름은 리턴한다.
+		Map.Entry<String,String> entry = map.entrySet().iterator().next();
+		answer = entry.getKey();
+		return answer;
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String[] part = {"min", "jang", "kim"}; 
+		String[] comp = {"kim", "jang"}; 
+		Hash01 sol = new Hash01(); 
+		System.out.println(sol.findPlayer(part, comp));
 	}
 
 }
