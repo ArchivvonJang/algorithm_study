0eef2b8ef164422be3ab84b667994cb965cfd253
minyun02 <www5981@naver.com>
2021-12-18 23:22:02 +0900

    k번째수 v1

---
+++ algorithm_study/src/yun/K번째수.java
@@ -0,0 +1,28 @@
+package yun;
+
+import java.util.Arrays;
+
+public class K번째수 {
+	public int[] solution(int[] array, int[][] commands) {
+		int[] answer = new int[commands.length];
+		for(int i=0; i<commands.length; i++) {
+//			int start = commands[i][0];
+//			int end = commands[i][1];
+//			int idx = commands[i][2]-1;
+			
+			int[] temp = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+			Arrays.sort(temp);
+			answer[i] = temp[commands[i][2]-1];
+		}
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		int[] array = {1, 5, 2, 6, 3, 7, 4};
+		int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
+		K번째수 k = new K번째수();
+		System.out.println(Arrays.toString(k.solution(array, commands)));
+	}
+
+}
+
