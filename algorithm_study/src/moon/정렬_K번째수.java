b34b564293a659e0c64e7dd7a981d60b321d575f
moonz <banan99@naver.com>
2021-12-18 13:39:18 +0900

    moon: 정렬_K번째수(2)

--- algorithm_study/src/moon/정렬_K번째수.java
+++ algorithm_study/src/moon/정렬_K번째수.java
@@ -4,15 +4,17 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-/* 배열 변경 불가 -> 리스트로 변환하는 방법 택함..
+/* 
 1. commands의 1차원 배열만큼 돌면서
 2. 2차원 배열의 i, j를 받아서 자르기 및 정렬
 3. List<Integer> -> int[]
 */
 public class 정렬_K번째수 {
     public int[] solution(int[] array, int[][] commands) {
+        /* 1번째 풀이
         int[] answer;
         List<Integer> list = new ArrayList<>();
+
         // 1.
         for (int a = 0; a<commands.length; a++) {
             // 2.
@@ -22,6 +24,21 @@ public class 정렬_K번째수 {
         }
         // 3.
         answer = list.stream().mapToInt(i->i).toArray();
+        return answer;
+    }*/
+
+    /* 2번째 풀이 - stream()을 알아보면서 3번이 아닌 2번에서 사용할 수 있겠다 생각하게 됨. */
+        int[] answer = new int[commands.length];
+        List<Integer> list = new ArrayList<>();
+        // 1.
+        for (int a = 0; a<commands.length; a++) {
+            // 2.
+            int[] sortedArr = Arrays.stream(array, commands[a][0]-1, commands[a][1])
+                    .sorted()
+                    .toArray();
+            answer[a] = (sortedArr[commands[a][2]-1]);
+        }
+        // 3.
         return answer;
     }
 
