f05c86c1a65a3b3689e649d203b3f60cb2caf917
minyun02 <www5981@naver.com>
2021-12-20 21:47:43 +0900

    k번째수 final

--- algorithm_study/src/yun/K번째수.java
+++ algorithm_study/src/yun/K번째수.java
@@ -6,10 +6,6 @@ public class K번째수 {
 	public int[] solution(int[] array, int[][] commands) {
 		int[] answer = new int[commands.length];
 		for(int i=0; i<commands.length; i++) {
-//			int start = commands[i][0];
-//			int end = commands[i][1];
-//			int idx = commands[i][2]-1;
-			
 			int[] temp = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
 			Arrays.sort(temp);
 			answer[i] = temp[commands[i][2]-1];
