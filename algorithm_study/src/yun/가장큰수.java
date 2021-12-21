a537fb7cea5a475f40966596a7060db6d13ce423
minyun02 <www5981@naver.com>
2021-12-21 22:12:23 +0900

    가장큰수 v1

---
+++ algorithm_study/src/yun/가장큰수.java
@@ -0,0 +1,16 @@
+package yun;
+
+public class 가장큰수 {
+	public String solution(int[] numbers) {
+		String answer = "";
+		
+		return answer;
+	}
+	
+	public static void main(String[] args) {
+		int[] numbers = {3, 30, 34, 5, 9};
+		가장큰수 k = new 가장큰수();
+		System.out.println("가장큰수=>"+k.solution(numbers));
+	}
+
+}
