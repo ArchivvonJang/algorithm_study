860a1ac3fbbce1737dad9e3f44a4722075b6dc74
moonz <banan99@naver.com>
2021-12-26 19:20:08 +0900

    정렬H-Index

---
+++ algorithm_study/src/moon/정렬_HIndex.java
@@ -0,0 +1,56 @@
+package moon;
+
+import java.util.Arrays;
+// 각 원소 : 각 논문의 인용 횟수
+// 전체 길이 : 논문 편수
+//  발표한 논문 n편 중, 논문 인용 횟수가 h번 이상인 논문들이 h개 이상. 그중 최대값이 h-Index
+
+public class 정렬_HIndex {
+    public int solution(int[] citations) {
+        int answer = 0;
+        Arrays.sort(citations);        // [0, 1, 3, 5, 6]
+        int h;
+        // 오름차순 정렬했으니까 앞에 있는 것보다 뒤에있는 수는 당연히 더 많은 횟수로 논문이 인용된 것이므로 논문 인용 횟수를 생각&비교할 필요가 없어진다.
+        // i    citations[i]      h (논문 편수)
+        // 0        0               5-0=5 : 0편 이상 인용된 논문 5개
+        // 1         1                5-1 =4
+        // 2        3                5-2 =3
+        //  논문 인용횟수가 h번 이상이어야 한다. 즉 h번 이상 인용된 논문이 h편 이상이어야함
+        for (int i=0; i < citations.length; i++) {
+            h = citations.length-i;  // 편 수
+            if (citations[i]>=h) {      // 논문 인용 횟수 citations[i] 가 h 이상인 논문 h편 이상
+                answer = h;      // h의 최댓값
+                break;
+            }
+        }
+        return answer;
+
+        // 첫번째 시도. test 16제외 실패..
+        // citaions 오름차순 정렬 -> 앞에서부터 하나씩 자신을 포함한 원소 갯수가 자신이랑 같은값들
+        /*
+        int citationsSize = citations.length;
+        int numCitations = 1;   // n번 이상 인용된 논문의 횟수 구할 때 자기 자신은 비교 필요없이 항상 포함이니까
+        int answer = 0;
+        Arrays.sort(citations);
+        for (int i=0; i<citationsSize; i++) {
+            for (int j= i+1; j<citationsSize; j++) {
+                if (citations[i] <= citations[j])
+                    numCitations++;
+            }
+            if (citations[i] == numCitations)
+                answer = citations[i];
+            numCitations = 1;
+        }
+
+//        Stream.of(citations)
+//                        .sorted().filter(numCitations -> numCitations == citationsSize)
+//        Arrays.stream(citations).sorted()
+           return answer;*/
+    }
+
+    public static void main(String[] args) {
+        정렬_HIndex k = new 정렬_HIndex();
+        int answer = k.solution(new int[]{4, 5, 6, 0, 1, 2, 10});  //3, 0, 6, 1, 5
+        System.out.println(answer);
+    }
+}
--- algorithm_study/src/moon/정렬_가장큰수.java
+++ algorithm_study/src/moon/정렬_가장큰수.java
@@ -1,11 +1,12 @@
 package moon;
-
 import java.util.Arrays;
 import java.util.Comparator;
+// 내림차순 정렬
+// 중복 o
 
 /*
 1.  int[]-> String[]로 변환
-2. 내림차순 정렬
+2. 내림차순 정렬          => a.compareTo(b) 메서드 : 앞에서부터 비교하다 다른 문자열이 나오면 'a-b' 순서로 해당 문자의 아스키코드 값을 뺀 결과(int)를 리턴
 3. 첫번째 자리가 0인 경우 return 0
 4. String[] -> String 변환
  */
@@ -22,7 +23,7 @@ public class 정렬_가장큰수 {
         Arrays.sort(stringNums, new Comparator<String>() {  //   함수형 인터페이스의 함수 객체
             @Override
             public int compare(String o1, String o2) {
-                return (o2 + o1).compareTo(o1 + o2);    // 내림차순
+                return (o2 + o1).compareTo(o1 + o2);    // 내림차순 : 아스키코드를 비교. o2+o1이 더 클 때 자리 exchange
                 // o1  o2
                 // 30 10
                 // 4   30
@@ -31,7 +32,7 @@ public class 정렬_가장큰수 {
                 // 3    4
             }
         });
-        //  첫번째 자리가 0이면 끝
+        // 모두 0일 경우("000") :  첫번째 자리가 0이면 0을 한개만 리턴하도록
         if (stringNums[0].equals("0")) return "0";
 
         // 0이 아니면 String 배열 -> String으로 변환
@@ -46,3 +47,8 @@ public class 정렬_가장큰수 {
         System.out.println(answer);
     }
 }
+
+/* CHECK
+- for 반복문 돌릴 시, 문자열 결합하면서(o2+o1) 새 인스턴스가 생성되어 비용이 발생하므로 StringBuilder를 사용해서 효율 ↑
+- Arrays.sort()와 Collections.sort() 차이는 없다. 사용 주체만 다를 뿐, 모두 퀵정렬 사용하여 시간 복잡도가 동일하다.
+ */
