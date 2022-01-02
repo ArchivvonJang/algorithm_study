bfd6da3b99e875a09f6590adf5037acfe80a9045
min <www5981@naver.com>
2022-01-02 18:18:48 +0900

    모의고사

---
+++ algorithm_study/src/yun/모의고사.java
@@ -0,0 +1,20 @@
+package yun;
+
+public class 모의고사 {
+	public int[] solution(int[] answers) {
+		int[] answer = {};
+		int[] s1 = {1, 2, 3, 4, 5};
+		int[] s2 = {2, 1, 2, 3, 2};
+		int[] s3 = {3, 3, 1, 1, 2};
+		
+		
+		
+		return answer;
+	}
+	public static void main(String[] args) {
+		모의고사 k = new 모의고사();
+		int[] answers = {1, 2, 3, 4, 5};
+		System.out.println(k.solution(answers));
+	}
+
+}
