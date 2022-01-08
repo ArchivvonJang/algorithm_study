b9b1cc99829528e854457ce2144970fcb9a2712b
min <www5981@naver.com>
2022-01-09 01:40:28 +0900

    타겟넘버

--- algorithm_study/src/yun/타켓넘버.java
+++ algorithm_study/src/yun/타켓넘버.java
@@ -6,14 +6,17 @@ public class 타켓넘버 {
     int target;
     int answer = 0;
 
-    public void dfs(int level, int sum){
+    public void dfs(int level, int sum, String test){
+//        System.out.println("level=>"+level);
+        System.out.println("test=>"+test);
+//        System.out.println("+++++++++++sum=>"+sum);
         if(level == numbers.length){
             if(sum == target){
                 answer++;
             }
         } else{
-            dfs(level+1, sum+numbers[level]);
-            dfs(level+1, sum-numbers[level]);
+            dfs(level+1, sum+numbers[level], "더하기");
+            dfs(level+1, sum-numbers[level], "빼기");
         }
     }
 
@@ -21,15 +24,15 @@ public class 타켓넘버 {
         this.numbers = numbers;
         this.target = target;
 
-        dfs(0, 0);
+        dfs(0, 0, "시작");
 
         return answer;
     }
 
     public static void main(String[] args) {
         타켓넘버 sol = new 타켓넘버();
-        int[] numbers = {1,1,1,1,1};
-        int target = 3;
+        int[] numbers = {1,1};
+        int target = 2;
         System.out.println(sol.solution(numbers, target));
     }
 }
