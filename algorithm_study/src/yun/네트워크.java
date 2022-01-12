31b98b5f347fc42401802712a9d5881b22eb893d
min <www5981@naver.com>
2022-01-12 18:07:21 +0900

    네트워크 - 큐

--- algorithm_study/src/yun/네트워크.java
+++ algorithm_study/src/yun/네트워크.java
@@ -12,38 +12,33 @@ public class 네트워크 {
 		visited = new boolean[n];
 		
 		for(int i=0; i<n; i++) {
-			if(visited[i]) { //true = 이미 방문한 노드 = 통
+			if(visited[i]) {
 				continue;
 			}
 			
-			//false인 노드(방문하지 않은)
 			Queue<Integer> q = new LinkedList<Integer>();
-			q.offer(i); // 큐에 => 0
-			visited[i] = true; //t, f, f
-			answer++;
+			q.offer(i);
+			visited[i] = true;
 			
 			//탐색 시작
 			while(!q.isEmpty()) {
-				int computer = q.poll(); // 0
+				int computer = q.poll();
 				
 				for(int j=0; j<n; j++) {
 					// 자기 자신이거나 방문한 노드면 건너뛰기
-					if(j == computer || visited[j]) { // 0 || true
+					if(j == computer || visited[j]) {
 						continue;
 					}
-					System.out.println("computer=>"+computer);
-					System.out.println("j=>"+j);
-					System.out.println("=>"+computers[computer][j]);
 					if(computers[computer][j] == 1) {
 						q.offer(j);
 						visited[j] = true;
 					}
 				}
 			}
+			answer++;
 		}
 		return answer;
 	}
-	
 	
 	public static void main(String[] args) {
 		네트워크 sol = new 네트워크();
