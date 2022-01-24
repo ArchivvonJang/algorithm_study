b1a25426ee69a1218bbf544090e78b59aa6b2b54
banan99 <banan99@naver.com>
2022-01-24 10:20:58 +0900

    해쉬_완주하지못한선수

--- algorithm_study/src/moon/해쉬_완주하지못한선수.java
+++ algorithm_study/src/moon/해쉬_완주하지못한선수.java
@@ -1,4 +1,5 @@
 package moon;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Iterator;
@@ -7,6 +8,9 @@ public class 해쉬_완주하지못한선수 {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
         Map<String, Integer> map = new HashMap<>();
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+
         // hashMap에 추가
         for (String p : participant) {
             map.put(p, map.getOrDefault(p, 0) + 1); // p가 key에 있으면 value를 가져옴. 없으면 디폴트 0
@@ -16,6 +20,7 @@ public class 해쉬_완주하지못한선수 {
             map.put(c, map.get(c)-1);
         }
         // 순환하면서 0인 선수 return
+        // Iterator가 더 속도높음
         Iterator<String> keys = map.keySet().iterator();
         while (keys.hasNext()) {
             String key = keys.next();
@@ -26,10 +31,21 @@ public class 해쉬_완주하지못한선수 {
         }
 
         // for (String key : map.keySet()) {
-        //     if (map.get(key) != 0)
+        //     if (map.get(key) != 0) {
         //         answer = key;
         //         break;
+        //     }
         // }
         return answer;
+    }
+
+    public static void main(String[] args) {
+        해쉬_완주하지못한선수 k = new 해쉬_완주하지못한선수();
+
+        String[] part = {"marina", "josipa", "nikola", "vinko", "filipa"};
+        String[] comp = {"josipa", "filipa", "marina", "nikola"};
+
+        String answer = k.solution(part, comp);
+        System.out.println(answer);
     }
 }
