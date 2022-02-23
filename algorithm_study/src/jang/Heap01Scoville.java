1fa84060384d140ff88127520879e35a33dbb0c5
ArchivvonJang <76926509+ArchivvonJang@users.noreply.github.com>
2022-02-23 20:07:18 +0900

    힙 - 더 맵게

--- algorithm_study/src/jang/Heap01Scoville.java
+++ algorithm_study/src/jang/Heap01Scoville.java
@@ -1,5 +1,6 @@
 package jang;
 
+import java.util.Arrays;
 import java.util.PriorityQueue;
 
 // 힙 - 더 맵게
@@ -16,22 +17,38 @@ public class Heap01Scoville {
 
         PriorityQueue<Integer> que = new PriorityQueue<>();
 
+        //1. 스코빌을 큐에 담아준다.
         for(int i = 0; i < scoville.length; i++){
             que.add(scoville[i]);
         }
 
+        //2. 큐에서 꺼낸 값이 K를 넘지 않는 한에서 반복
         while (que.peek() < K){
-            //음식 중에 섞어도 K 이상의 스코빌 지수를 가질 수 없으면 -1을 반환
+
+            //3. 모든 음식의 스코빌 지수를 K 이상으로 만들 수 없는 경우에는 -1을 return
             if(que.size() < 2) return -1;
 
+            //4. a와 b를 큐에서 꺼낸 후
             int a = que.poll();
             int b = que.poll();
 
-            que.add(a + (b * 2));
+            //5. 스코빌 지수를 계산하고
+            que.offer(a + (b * 2));
 
+            //6. K 이상이 되면 answer +1 반복문을 나온다.
             answer++;
         }
 
         return answer;
     }
+
+    public static void main(String[] args) {
+
+        int[] scoville = new int[] {1, 2, 3, 9, 10, 12 };
+        int k = 7;
+        Heap01Scoville h = new Heap01Scoville();
+        System.out.println(h.solution(scoville, k));
+
+    }
+
 }
